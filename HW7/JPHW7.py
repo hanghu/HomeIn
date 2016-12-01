c1a4ff1ffa12f1a64db13f70ed7b7296899f2d47
jhp312 <puleojh@gmail.com>
2016-11-30 17:54:13 -0800

    " HW7: Deleted unused module os, Corrected file paths, Shortened length of lines, used # instead of ‘’’ for single line docs, Fixed indentation of code "

---
+++ HW7/JPHW7.py
@@ -0,0 +1,51 @@
+## Folium mapping
+
+'''Populating the map with clusters of houses and a heat map of crime rates.'''
+
+
+#import data manipulators
+import pandas as pd
+
+#import folium plugins
+from folium.plugins import MarkerCluster
+import folium
+from folium import plugins
+
+#load King County datasets
+crimes = pd.read_csv('../data/King_County_Sheriff_s_Office.csv')
+houses = pd.read_csv('../data/kc_house_data.csv', parse_dates=['date'])
+
+#prepare base map
+'''Use mean of latitude and longitude in dataset and set original zoom'''
+kingcounty_map = folium.Map(location = [houses['lat'].mean(),
+	houses['long'].mean()], zoom_start = 10)
+kingcounty_map.save('kingcounty_map.html')
+
+#group together houses
+house_groups = folium.MarkerCluster().add_to(kingcounty_map)
+
+'''Iterate through rows and create marker clusters.
+	popup datatypes:
+		price: float64
+		size: int64
+		year built: int64'''
+
+for name, row in houses[0:200].iterrows():
+    folium.Marker([row["lat"], row["long"]],
+                  popup="Price: {0}$,Size: {1}, Year Built: {2}"\
+                  .format(row["price"],row['sqft_living'], row['yr_built'])).add_to(house_groups)
+
+#save output of house markers
+kingcounty_map.save('houses_map.html')
+
+#Create new basemap for crimes heat map
+kccrimes_heatmap = folium.Map(location = [houses['lat'].mean(), houses['long'].mean()], zoom_start = 10)
+
+#Add data to heat map and select opacity
+kccrimes_heatmap.add_children(
+    plugins.HeatMap(
+    	[[row["latitude"], row["longitude"],row["city"]]for iters, row in crimes[0:100].iterrows()],
+    	min_opacity=1, max_zoom=10, radius = 8))
+
+#save output of King County heat map
+kccrimes_heatmap.save('kingcounty_map.html')
