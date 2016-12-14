34468176328a1e9a48ed34faa1e036197144fa10
jhp312 <puleojh@gmail.com>
2016-12-14 15:18:25 -0800

    "updated tests"

--- HomeIn/test_zipcodes.py
+++ HomeIn/test_zipcodes.py
@@ -5,13 +5,17 @@ import numpy as np
 from geopy.geocoders import Nominatim
 from zipcodes_search import *
 
-zipcodes = []
-locations = [("47.500626, -122.234039")]
+
 class TestZipcodes(unittest.TestCase):
 	def test_geocode(self):
-		geocode()
+		location = geolocator.reverse("47.500626, -122.234039")
+		add = location.address
+		splita = add.rsplit(', ', 1)
+		splitb = splita[0]
+		splitc = splitb.rsplit(', ', 1)
+		code = splitc[1]
        	result = '98178'
-        self.assertEqual(zipcodes, result)
+        self.assertEqual(code, result)
 
 if __name__ == '__main__':
     unittest.main()
