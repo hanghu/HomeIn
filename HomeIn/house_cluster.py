b82cee691dbd0582399c3ac7a96aea87ace35cc3
MengyuanZoe <liumyzoe@uw.edu>
2016-12-13 16:09:58 -0800

    add house street view into house_cluster.py

--- HomeIn/house_cluster.py
+++ HomeIn/house_cluster.py
@@ -17,24 +17,19 @@ MAX_SHOW = 10
 TYPE_OF_CRIME = 6
 
 def popup_text(iters, row):
-	TmpFig = str(iters) + ".png"
+	
+	#TmpFig = str(iters) + ".png"
+	#fig,ax = plt.subplots()
+	#ax.bar(range(6), [row['Assault'],row['Breaking & Entering'],row['Drugs'],row['Property Crime'],row['Robbery'],row['Theft']])
+	#fig.savefig(TmpFig)
 
-	fig,ax = plt.subplots()
-	ax.bar(range(6), [row['Assault'],row['Breaking & Entering'],row['Drugs'],row['Property Crime'],row['Robbery'],row['Theft']])
-	fig.savefig(TmpFig)
+	url = "https://maps.googleapis.com/maps/api/streetview?size=300x300&location=" + str(row['lat']) + "," + str(row['long']) + "&pitch=10&key=AIzaSyALN4DKjGjGkg_KA7XFvLSCBlBOMATQdxE"
 	
 	html = """
-		<h4> Crime Info within Neighborhood </h4><br>
+		<h4> House Street View </h4><br>
 		"""
-#<img src='file://' + os.path.realpath(TmpFig) alt="Crime Histogram" style="width:304px;height:228px;"> <br>
-	html = html + "Total Crimes: " + str(row['number of crimes']) + "<br>"
-	html = html + "Assault: " + str(row['Assault']) + "<br>"
-	html = html + "Breaking & Entering: " + str(row['Breaking & Entering']) + "<br>"
-	html = html + "Drugs: " + str(row['Drugs']) + "<br>"
-	html = html + "Property Crime: " + str(row['Property Crime']) + "<br>"
-	html = html + "Robbery: " + str(row['Robbery']) + "<br>"
-	html = html + "Theft: " + str(row['Theft']+row['Theft from Vehicle']+row['Theft of Vehicle']) + "<br>"
-	#html = html + <img src="./tmp.png" alt="Crime Histogram" style="width:304px;height:228px;"> + <br>;
+
+	html = html + "<img src =" + url + ">" + "<br>"
 	html = html + "<h4> House Info </h4><br>"
 	html = html + "Sqft of Living Area: " + str(row['sqft_living']) + "<br>"
 	html = html + "Sqft of Lot:imesiik " + str(row['sqft_lot']) + "<br>"
@@ -42,21 +37,16 @@ def popup_text(iters, row):
 	html = html + "Bedroom/Bathroom: " + str(row['bedrooms']) + "Bed/" + str(row['bathrooms']) + "Bath" + "<br>"
 	html = html + "Year Built: " + str(row['yr_built']) + "<br>"
 	html = html + "Year Renovated: " + str(row['yr_renovated']) + "<br>"
-
-	# bar plot'file://' + os.path.realpath(TmpFig)
-	#BarPlot = vincent.Bar([10, 20, 30, 20, 15, 30, 45], width = 150, height = 50)
-	#BarPlot.axis_titles(x='Crime Type', y='Number of Incidents')
-	#rotate x axis labels
-	#ax = AxisProperties(
-    #    labels = PropertySet(angle=ValueRef(value=270)))
-	#BarPlot.axes[0].properties = ax
-	#folium.Vega(BarPlot, width = 300, height = 100).add_to(popup)
+	html = html + "<h4> Crime Info within Neighborhood </h4><br>"
+	html = html + "Total Crimes: " + str(row['number of crimes']) + "<br>"
+	html = html + "Assault: " + str(row['Assault']) + "<br>"
+	html = html + "Breaking & Entering: " + str(row['Breaking & Entering']) + "<br>"
+	html = html + "Drugs: " + str(row['Drugs']) + "<br>"
+	html = html + "Property Crime: " + str(row['Property Crime']) + "<br>"
+	html = html + "Robbery: " + str(row['Robbery']) + "<br>"
+	html = html + "Theft: " + str(row['Theft']+row['Theft from Vehicle']+row['Theft of Vehicle']) + "<br>"
 
 	iframe = folium.element.IFrame(html = html, width = 500, height = 300)
-	#BarFigure = folium.element.Figure()
-	#BarPlot.add_to(BarFigure)
-	#BarFigure.add_to(iframe)
-
 	popup = folium.Popup(iframe, max_width = 2650)
 
 	return popup
