1fd16a553ea95a992736bcef37ddfdfa1671988a
jhp312 <puleojh@gmail.com>
2016-12-14 21:51:30 -0800

    "added unit test for zipcodes"

--- HomeIn/test_zipcodes.py
+++ HomeIn/test_zipcodes.py
@@ -1,21 +1,19 @@
-from time import sleep
 import unittest
-import pandas as pd
-import numpy as np
 from geopy.geocoders import Nominatim
-from zipcodes_search import *
-
 
 class TestZipcodes(unittest.TestCase):
-	def test_geocode(self):
+
+	def testgeocode(self):
+		'''Test an instance of zipcode retrieval'''
+		geolocator = Nominatim()
 		location = geolocator.reverse("47.500626, -122.234039")
 		add = location.address
 		splita = add.rsplit(', ', 1)
 		splitb = splita[0]
 		splitc = splitb.rsplit(', ', 1)
 		code = splitc[1]
-       	result = '98178'
-        self.assertEqual(code, result)
+		result = '98178'
+		self.assertEqual(code, result)
 
 if __name__ == '__main__':
     unittest.main()
